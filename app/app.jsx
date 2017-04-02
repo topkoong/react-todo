33962bfb0e6569ede9c01febbe7ddc790dd6ae46
topkoong <engineer_top@msn.com>
2017-04-02 00:03:25 -0400

    Add TodoApp component

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 //This is exactly the same below, creating new variable Route = require('react-router').Route;
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');// all the properties we wanna pull off
-
+var TodoApp = require('TodoApp');
 
 // loading CSS (foundation)
 require('style!css!foundation-sites/dist/css/foundation.min.css'); // css!, meaning we use css loader that we installed
@@ -21,6 +21,6 @@ require('style!css!sass!applicationStyles');
 //var {name} = obj; // This creates a new variable called name and sets it to equal to whatever obj.name equals to.
 
 ReactDOM.render(
-  <p>Boilerplate 3 project</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
