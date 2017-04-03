2c9016bbdc5543c80ea6e74320c91ddf3a05b456
topkoong <engineer_top@msn.com>
2017-04-02 23:03:11 -0400

    Add Search component

---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+//onChange, The onchange event occurs when the value of an element has been changed.
+var TodoSearch = React.createClass({
+  handleSearch: function () {
+    var showCompleted = this.refs.showCompleted.checked;
+    var searchText = this.refs.searchText.value; // grab value
+
+    this.props.onSearch(showCompleted, searchText);
+  },
+  render: function () {
+    return (
+      <div>
+        <div>
+          <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+            Show completed todos
+          </label>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoSearch;
